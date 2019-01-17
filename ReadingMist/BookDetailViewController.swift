b33b2ee78bfc2973502cf7085fb02349c08aa590
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-17 17:25:11 -0600

    Implement BookDetailViewController's saveBarButtonTapped and updateViews functions

--- ReadingMist/BookDetailViewController.swift
+++ ReadingMist/BookDetailViewController.swift
@@ -10,20 +10,45 @@ import UIKit
 
 class BookDetailViewController: UIViewController {
     
+    var bookController: BookController?
+    var book: Book? {
+        didSet { updateViews() }
+    }
+    
     @IBOutlet weak var bookTitleTextField: UITextField!
     @IBOutlet weak var reasonToReadTextView: UITextView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
     }
     
     
     @IBAction func saveBarButtonTapped(_ sender: Any) {
+        guard let bookController = bookController,
+            let title = bookTitleTextField.text, !title.isEmpty,
+            let reason = reasonToReadTextView.text else { return }
         
+        if let book = book {
+            bookController.update(book: book, title: title, reasonToRead: reason)
+        } else {
+            bookController.createBook(with: title, reasonToRead: reason)
+        }
     }
     
-    
-
+    private func updateViews() {
+        if let book = book {
+            bookTitleTextField.text = book.title
+            reasonToReadTextView.text = book.reasonToRead
+            navigationItem.title = book.title
+        } else {
+            navigationItem.title = "Add a new book"
+        }
+    }
 }
