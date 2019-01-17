3bb036c6e27c74ae59cf7ad17a613eeb8b44e782
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2019-01-17 15:35:07 -0600

    Lay out storyboard, add outlets/actions and constraint them. Create subclass for each scene, and make connections

---
+++ ReadingMist/BookDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BookDetailViewController.swift
+//  ReadingMist
+//
+//  Created by Ilgar Ilyasov on 1/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var reasonToReadTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func saveBarButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+
+}
---
+++ ReadingMist/BookTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookTableViewCell.swift
+//  ReadingMist
+//
+//  Created by Ilgar Ilyasov on 1/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var hasBeenReadButton: UIButton!
+    
+    @IBAction func hasBeenReadButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+
+}
